f355ad2f6fdb0ca5c8e7c2ef349231d98b5c6183
soheyHashimoto <heyso1209@gmail.com>
2021-10-12 10:17:21 +0900

    [ADD]完成

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,11 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
-
-    println!("The secret number is: {}", secret_number);
 
     loop {
         println!("Please input your guess.");
@@ -18,9 +16,10 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-
-        let guess: u32 = guess.trim().parse()
-            .expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {}", guess);
 
