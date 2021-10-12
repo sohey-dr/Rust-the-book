ae2c47216848dc32bfd24164312d9bc0d6e35cb7
soheyHashimoto <heyso1209@gmail.com>
2021-10-12 18:57:27 +0900

    [ADD]for

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,11 +1,6 @@
 fn main() {
-    let condition = true;
-    let number = if condition {
-        5
-    } else {
-        6
-    };
-
-    // numberの値は、{}です
-    println!("The value of number is: {}", number);
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
 }
