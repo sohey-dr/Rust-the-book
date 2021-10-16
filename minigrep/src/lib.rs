9a3d1be2afcd3aa89fb594c0c8546267f2d6e2ca
soheyHashimoto <heyso1209@gmail.com>
2021-10-17 01:17:08 +0900

    [ADD]search_case_insensitiveのテスト

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -64,19 +64,38 @@ mod test {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
-        // Rustは
-        // 安全で速く生産性も高い。
-        // 3つ選んで。
+// Rust
+// 安全かつ高速で生産的
+// 三つを選んで
+// ガムテープ
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
         assert_eq!(
             vec!["safe, fast, productive."],
             search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+// (最後の行のみ)
+// 私を信じて
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
         );
     }
 }
