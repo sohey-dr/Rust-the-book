892f6710f1a160ec7a417dc7ec8dc2a67a076450
soheyHashimoto <heyso1209@gmail.com>
2021-10-17 01:10:33 +0900

    [ADD]testを成功させるためにsearch関数を実装

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -31,8 +31,17 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
+
 
 #[cfg(test)]
 mod test {
