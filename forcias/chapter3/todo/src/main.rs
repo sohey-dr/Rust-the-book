51f291e1ac5ec514e9d3f31dea160a6823b650fd
soheyHashimoto <heyso1209@gmail.com>
2021-10-30 02:54:48 +0900

    [ADD]html render

--- forcias/chapter3/todo/src/main.rs
+++ forcias/chapter3/todo/src/main.rs
@@ -1,15 +1,40 @@
 use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
+use askama::Template;
 use thiserror::Error;
 
+struct TodoEntry {
+    id: u32,
+    text: String,
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {
+    entries: Vec<TodoEntry>,
+}
+
 #[derive(Error, Debug)]
-enum MyError {}
+enum MyError {
+    #[error("Todo with id {0} not found")]
+    AskamaError(#[from] askama::Error),
+}
 
 impl ResponseError for MyError {}
 
 #[get("/")]
-async fn index() -> Result<HttpResponse, actix_web::Error> {
-    let response_body = "Hello World!";
-    Ok(HttpResponse::Ok().body(response_body))
+async fn index() -> Result<HttpResponse, MyError> {
+    let mut entries = Vec::new();
+    entries.push(TodoEntry {
+        id: 1,
+        text: "Learn Rust".to_string(),
+    });
+    entries.push(TodoEntry {
+        id: 2,
+        text: "Learn Actix".to_string(),
+    });
+    let html = IndexTemplate { entries };
+    let response_body = html.render()?;
+    Ok(HttpResponse::Ok().content_type("text/html").body(response_body))
 }
 
 #[actix_web::main]
