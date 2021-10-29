85282474387ab1ed71da0790fb1df0c8d0f47195
soheyHashimoto <heyso1209@gmail.com>
2021-10-30 02:06:50 +0900

    [ADD]clapを使ってコマンド引数取得

--- forcias/chapter2/samplecli/src/main.rs
+++ forcias/chapter2/samplecli/src/main.rs
@@ -1,6 +1,31 @@
-use std::env;
+use clap::{App, Arg};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let matches = App::new("My PRN program")
+        .version("1.0")
+        .author("Your name")
+        .about("Super awesome sample PRN calculator")
+        .arg(
+            Arg::new("formula_file")
+                .about("Formulas written in PRN")
+                .value_name("FILE")
+                .index(1)
+                .required(false),
+        )
+        .arg(
+            Arg::new("output_file")
+                .about("Output file")
+                .value_name("FILE")
+                .index(2)
+                .required(false),
+        )
+        .get_matches();
+
+    match matches.value_of("formula_file") {
+        Some(file) => println!("Using formula file: {}", file),
+        None => println!("No formula file"),
+    }
+
+    let verbose = matches.is_present("verbose");
+    println!("Verbose: {}", verbose);
 }
