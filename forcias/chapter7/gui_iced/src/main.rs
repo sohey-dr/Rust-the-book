25b7a1fc8d0c087470144a6357b8ca9e14c93655
soheyHashimoto <heyso1209@gmail.com>
2021-10-30 23:58:19 +0900

    [ADD]GUI上にHello Worldを出力する

--- forcias/chapter7/gui_iced/src/main.rs
+++ forcias/chapter7/gui_iced/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{executor, Application, Clipboard, Command, Element, Settings, Text};
+
+struct GUI;
+
+impl Application for GUI {
+    type Executor = executor::Default;
+    type Message = ();
+    type Flags = ();
+
+    fn new(_flags: ()) -> (Self, Command<Self::Message>) {
+        (GUI, Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("Forcias")
+    }
+
+    fn update(
+        &mut self,
+        _message: Self::Message,
+        _clipboard: &mut Clipboard,
+    ) -> Command<Self::Message> {
+        Command::none()
+    }
+
+    fn view(&mut self) -> Element<Self::Message> {
+        Text::new("Hello, world!").into()
+    }
+}
+
+fn main() ->iced::Result {
+    GUI::run(Settings::default())
 }
